e94503b071ea84fe7ce211a910698b83c2412002
Melih Teke <36086368+melihteke@users.noreply.github.com>
2023-05-26 23:19:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wtisdk',
-    version='1.2',
+    version='1.3',
     author='Melih Teke',
     author_email='me@mteke.com',
     description='WTI SDK for interacting with WTI devices',
